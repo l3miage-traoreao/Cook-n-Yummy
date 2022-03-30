2031e13a1bf32b39d0fe9a3fb47a691f37ac2403
traoreao <traoreao@im2ag-imac-06.e-im2ag.ujf-grenoble.fr>
2022-03-30 11:00:58 +0200

    initialisation

---
+++ Cook'n Yummy/Cook_n_YummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cook_n_YummyApp.swift
+//  Cook'n Yummy
+//
+//  Created by traoreao on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cook_n_YummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
