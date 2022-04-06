eee3e08cbec6530ec58d496c203c176f6c17cafd
traoreao <traoreao@master-imac.e-im2ag.ujf-grenoble.fr>
2022-04-06 11:20:22 +0200

    les 2 premieres vues

---
+++ Cooknyummy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cooknyummy
+//
+//  Created by traoreao on 06/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
